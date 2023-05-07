608a38c36dbf7e7f7448b787e6784aa166dc44ec
Prajwol Acharya <87631485+prjla35@users.noreply.github.com>
2023-05-07 20:36:25 +0545

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,8 +3,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = $_POST['message'];
     $to = 'prjla35@gmail.com';
     $subject = 'New message from your website';
-    $headers = 'From: your-website@domain.com' . "\r\n" .
-               'Reply-To: your-website@domain.com' . "\r\n" .
+    $headers = 'From: https://prjla35.github.io/animex/' . "\r\n" .
+               'Reply-To: prjla36@gmail.com' . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
 
     if (mail($to, $subject, $message, $headers)) {
